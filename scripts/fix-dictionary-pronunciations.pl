a043f9c6dfbfda5a859dc46bd125c8366b9ce2b0
Jim O'Regan <jaoregan@tcd.ie>
2017-12-08 00:13:00 +0000

    mid change

--- scripts/fix-dictionary-pronunciations.pl
+++ scripts/fix-dictionary-pronunciations.pl
@@ -8,7 +8,9 @@ binmode(STDIN, ":utf8");
 binmode(STDOUT, ":utf8");
 
 my %fixU = (
-    'éigin' => 'inteacht'
+    'éigin' => 'inteacht',
+    'a Dhia' => 'Dhia',
+    'a fhad le' => 'fhad le',
 );
 my %fixC = (
     'éigin' => 'eicint',
@@ -22,6 +24,7 @@ my %fixC = (
 my %fixM = (
     'éigin' => 'éigint',
     'fáil' => 'fáilt',
+    'a fhad le' => 'fad le',
     # zónáilte = zónáilthe
 );
 
@@ -51,14 +54,55 @@ sub get_replacement {
     }
 }
 
+sub irish_lc {
+    my $text = shift;
+    $text =~ s/^([nt])([AEIOUÁÉÍÓÚ].*)/$1-$2/;
+    return lc($text);
+}
+
+sub tolower {
+    my $text = $_[0];
+    my @pieces = split / /, $text;
+    my @lcpieces = map { irish_lc($_) } @pieces;
+    return join(" ", @lcpieces);
+}
+
+my $out = 1;
+my $spk = 1;
+
+if (! -d "audio") {
+    mkdir "audio";
+}
+if (! -d "data") {
+    mkdir "data";
+}
+if (! -d "data/train") {
+    mkdir "data/train";
+}
+
+open(UTT, '>', 'data/train/utt2spk');
+open(TEXT, '>', 'data/train/text');
+binmode(TEXT, ":utf8");
+open(URLS, '>', 'audio/urls');
+open(WAVSCP '>', 'data/train/wav.scp');
+
 while(<>) {
     chomp;
     my $dialect = '';
-    if (/\/Can([CUM])\//) {
+    my ($file, $speaker) = split/\t/;
+    if ($speaker eq '') {
+        $speaker = "unk$spk";
+        $spk++;
+    }
+    if ($file =~ /\/Can([CUM])\//) {
         $dialect = $1;
     }
-    s/\.mp3$//;
-    my $text = '';
+    my $base = $file;
+    $base =~ s/\.mp3$//;
+    my $outfile = $base . ".txt";
+    open(OUT, '>', "$outfile");
+    binmode(OUT, ":utf8");
+
     if(/\/([^\/]*)$/) {
         $text = $1;
     }
@@ -66,11 +110,12 @@ while(<>) {
         if($dialect ne 'M' || ($dialect eq 'M' && ($text ne 'éigin' && $text ne 'am éigin'))) {
             my $replacement = get_replacement('éigin', $dialect);
             $text =~ s/éigin/$replacement/;
-            print "$text\n";
+            print OUT tolower("$text\n");
         } else {
-            print "$text\n";
+            print OUT tolower("$text\n");
         }
     } else {
-        print get_replacement($text, $dialect) . "\n";
+        print OUT tolower(get_replacement($text, $dialect)) . "\n";
     }
+    close(OUT);
 }
