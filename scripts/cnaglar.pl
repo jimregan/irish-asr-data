f4125f699e7e0678bfe89f3e22e9b8ebde4af29b
Jim Regan <joregan@gmail.com>
2017-12-12 22:55:21 +0000

    [WIP] cnaglarl (#3)

---
+++ scripts/cnaglar.pl
@@ -0,0 +1,91 @@
+#!/usr/bin/perl
+# Copyright (c) 2017 Jim O'Regan
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+use warnings;
+use strict;
+use utf8;
+
+use URI;
+use Web::Scraper;
+use Encode;
+use Data::Dumper;
+
+binmode(STDOUT, ":utf8");
+
+my $index_scraper = scraper {
+    process '//ul', 'spans[]' => scraper {
+        process 'li', 'items[]' => scraper {
+                process 'a', 'link' => '@href';
+        };
+    };
+};
+
+#my $index = URI->new("https://www.scss.tcd.ie/~uidhonne/comhra/index.utf8.html");
+
+#my $idxres = $index_scraper->scrape($index);
+
+#my @pages = ();
+
+#for my $span (@{$idxres->{'spans'}}) {
+#    for my $item (@{$span->{'items'}}) {     
+#            if(exists $item->{'link'} && $item->{'link'} =~ m!/comhra/!) {
+#                push @pages, $item->{'link'};
+#            }
+#    }
+#}
+
+sub extract_mp3 {
+    if(exists $_[0]->{'_content'}) {
+        my @content = @{$_[0]->{'_content'}};
+        for my $script (@content) {
+            if($script =~ /mp3: "([^"]*)",/) {
+                return $1;
+            }
+        }
+    }
+}
+
+my $content_scraper = scraper {
+    process '//script[count(@src) = 0 and contains(text(),"mp3:")]', 'script[]' => [sub { extract_mp3(@_); }];
+    process '//div[@id = "text"]', 'text' => scraper {
+        process 'dl', 'chunk[]' => scraper {
+            process 'dt', 'speaker' => 'TEXT';
+            process 'dd', 'segments[]' => scraper {
+                process 'p', 'start' => '@start';
+                process 'p', 'end' => '@end';
+                process 'p', 'text' => 'TEXT';
+            };
+        };
+    };
+};
+
+print Dumper $res;
+
+sub proc_page {
+    my $page = shift;
+    my $base = 'https://www.scss.tcd.ie/~uidhonne/comhra/';
+    my $uri = URI->new($page);
+    my $id = '';
+    if($page =~ m!/comhra/([^.]*)\.!) {
+        $id = $1;
+    }
+    my $res = $content_scraper->scrape($uri);
+}
---
+++ scripts/vtt-patcher.pl
@@ -0,0 +1,96 @@
+#!/usr/bin/perl
+# Copyright 2017 Jim O'Regan
+# Apache 2.0
+
+open(VTT, $ARGV[0]);
+open(IN, $ARGV[1]);
+binmode(STDOUT, ":utf8");
+binmode(VTT, ":utf8");
+binmode(IN, ":utf8");
+
+my %vtt = ();
+my %starts = ();
+my %ends = ();
+
+my $key = '';
+my $newkey = '';
+my $text = '';
+while(<VTT>) {
+    chomp;
+    s/\r//g;
+    next if(/^WEBVTT/);
+    next if(/X-TIMESTAMP/);
+    next if(!$expecting && /^$/);
+    if(/(\d{2}:\d{2}:\d{2}\.\d{3}) --> \d{2}:\d{2}:\d{2}\.\d{3}/) {
+        $newkey = $1;
+        $expecting = 1;
+        $text =~ s/  +/ /g;
+        $vtt{$key} = $text;
+        $key = $newkey;
+        $text = '';        
+    } else {
+        if($text eq '') {
+            $text = $_;
+        } else {
+            $text .= " $_";
+        }
+    }
+}
+
+my $previd = '';
+my $line = 0;
+while(<IN>) {
+    chomp;
+    $line++;
+    next if(/^#/);
+    my @pieces = split/\t/;
+    if($#pieces == 1 && $pieces[1] eq 'd') {
+        delete $vtt{$pieces[0]};
+        $previd = '';
+    } elsif($#pieces == 2 && $pieces[1] eq 's') {
+        my $tmp = $vtt{$pieces[0]};
+        my $split = quotemeta($pieces[2]);
+        my @prts = split/$split/, $tmp;
+        for (my $i = 0; $i <= $#prts; $i++) {
+            my $id = $pieces[0] . "-" . ($i + 1);
+            $vtt{$id} = $prts[$i];
+        }
+        delete $vtt{$pieces[0]};
+        # Deliberately not updating $previd; 'merge with previous' doesn't want this
+    } elsif($#pieces == 1 && $pieces[1] eq 'mp') {
+        if($previd eq '') {
+            die "previd unset";
+        }
+        $vtt{$previd} .= " $vtt{$pieces[0]}";
+        delete $vtt{$pieces[0]};
+    } else {
+        if($#pieces < 3) {
+            die "Error on line $line: $_\n";
+        }
+        my $id = $pieces[0];
+        my $text = $vtt{$id};
+        my $speaker = $pieces[1];
+        my $start = $pieces[2];
+        my $end = $pieces[3];
+        if($#pieces > 3) {
+            for(my $i = 4; $i <= $#pieces; $i += 2) {
+                my $what = quotemeta($pieces[$i]);
+                my $with = '';
+                if($i == $#pieces && $#pieces % 2 != 1) {
+                    $with = '';
+                } else {
+                    $with = $pieces[$i + 1];
+                }
+                $text =~ s/$what/$with/g;
+            }
+        }
+        $vtt{$id} = $text;
+        $previd = $id;
+        $starts{$id} = $start;
+        $ends{$id} = $end;
+    }
+}
+
+for my $s (sort { $vtt{$a} <=> $vtt{$b} } keys %vtt) {
+    print "$s: $starts{$s} $ends{$s} $vtt{$s}\n";
+}
