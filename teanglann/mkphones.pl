d8f3fa6841406cf4d2886fd271ed5d65e436bf0e
Jim O'Regan <jaoregan@tcd.ie>
2017-12-13 00:01:11 +0000

    make phones from dict, because uniq deletes a lot of crap with IPA(!)

---
+++ teanglann/mkphones.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use utf8;
+
+my %phones = ();
+
+binmode(STDIN, ":utf8");
+binmode(STDOUT, ":utf8");
+
+while(<>) {
+    chomp;
+    my ($a, $b) = split/\t/;
+    my @phn = split/ /, $b;
+    for my $phone (@phn) {
+        if(!exists $phones{$phone}) {
+            $phones{$phone} = 1;
+            print "$phone\n";
+        }
+    }
+}
