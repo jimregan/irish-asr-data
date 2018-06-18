94bac5935958dc4cadb9e4257dcba5d411bae6ec
Jim O'Regan <jaoregan@tcd.ie>
2018-06-18 03:49:58 +0100

    missed some stories

--- scripts/download-aesop.sh
+++ scripts/download-aesop.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-male="leon-agus-an-luch an-fearr-agus-a-bheirt-inionacha an-ghaoth-aagus-an-ghrian an-cailin-agus-cearca ghiorrai-agus-a-chairde"
-female="coileach-agus-an-madra"
+male="leon-agus-an-luch an-fearr-agus-a-bheirt-inionacha an-ghaoth-aagus-an-ghrian an-cailin-agus-cearca ghiorrai-agus-a-chairde luch-tuaithe-agus-an-luch-baile cat-agus-na-lucha"
+female="coileach-agus-an-madra cat-agus-an-sionnach madra-sa-mhainsear"
 for i in $male $female
 do
 	swf=$(lynx -source http://leighleat.com/$i.html|grep '<param name="movie"'|awk -F'"' '{print $4}')
