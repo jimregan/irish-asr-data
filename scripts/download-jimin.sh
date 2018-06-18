1344d51a7cb5054a0fc659d0f12f49172ed245b0
Jim O'Regan <jaoregan@tcd.ie>
2018-06-18 01:55:43 +0100

    script to download Jimin

---
+++ scripts/download-jimin.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Chapters 5 & 6 are missing
+for i in 1 2 3 4 7 8 9 10 11 12
+do
+	swf=$(lynx -source http://leighleat.com/caib-$i.html|grep '<param name="movie"'|awk -F'"' '{print $4}')
+	wget http://leighleat.com/$swf -O jimin$i.swf
+	perl extract-swf-mp3.pl jimin$i.swf
+	rm jimin$i.swf
+done
+	
