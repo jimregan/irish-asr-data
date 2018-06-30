b016298adfa54380eb76f2ad60a9abcdca092158
Jim O'Regan <jaoregan@tcd.ie>
2018-06-30 14:55:36 +0100

    at least one of the pages has the content repeated; files identical

--- scripts/daltai-proverbs-scraper.sh
+++ scripts/daltai-proverbs-scraper.sh
@@ -6,9 +6,9 @@ do
 	url="http://www.daltai.com/proverbs/$u/"
 	lynx -dump $url|grep "$url"|awk '{print $2}'|grep -v '/#'|grep -v "^$url$"|sort|uniq | while read i
 	do
-		head=$(lynx -source $i/|grep '<h1>' |awk -F'[<>]' '{print $3}')
-		url=$(lynx -source $i/|grep '/tinymce/jscripts/'|awk -F'"src":"' '{print $2}'|awk -F'"' '{print "http://www.daltai.com"$1}')
-		grep $url daltai.txt || printf "%08d\t%s\t%s\t%s\n" $sent $u "$head" $url >> daltai.txt
+		head=$(lynx -source $i/|grep '<h1>' |awk -F'[<>]' '{print $3}'|head -n 1)
+		url=$(lynx -source $i/|grep '/tinymce/jscripts/'|awk -F'"src":"' '{print $2}'|awk -F'"' '{print "http://www.daltai.com"$1}'|head -n 1)
+		grep $url daltai.txt || printf "%08d\t%s\t%s\t%s\n" $sent $i "$head" $url >> daltai.txt
 		sent=$(($sent + 1))
 	done
 done
