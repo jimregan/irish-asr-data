2d4a107e3c0acd2248fdd11f244317087db5c54c
Jim O'Regan <jaoregan@tcd.ie>
2018-06-30 14:32:39 +0100

    scraper for daltai.com proverbs

---
+++ scripts/daltai-proverbs-scraper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sent=1
+for u in seanfhocal-na-seachtaine living-and-dying material-things personal-qualities-types-of-people relationships-dealing-with-others the-bigger-world
+do
+	url="http://www.daltai.com/proverbs/$u/"
+	lynx -dump $url|grep "$url"|awk '{print $2}'|grep -v '/#'|grep -v "^$url$"|sort|uniq | while read i
+	do
+		head=$(lynx -source $i/|grep '<h1>' |awk -F'[<>]' '{print $3}')
+		printf "%08d\t%s\n" $sent "$head" >> daltai-transcript.txt
+		url=$(lynx -source $i/|grep '/tinymce/jscripts/'|awk -F'"src":"' '{print $2}'|awk -F'"' '{print "http://www.daltai.com"$1}')
+		printf "%08d\t%s\n" $sent $url >> daltai-urls.txt
+		sent=$(($sent + 1))
+	done
+done
