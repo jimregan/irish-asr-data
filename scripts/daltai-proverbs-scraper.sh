cfa2d3104ab29ba735feab999de82bfa1c1ebac3
Jim O'Regan <jaoregan@tcd.ie>
2018-06-30 14:47:03 +0100

    track original url

--- scripts/daltai-proverbs-scraper.sh
+++ scripts/daltai-proverbs-scraper.sh
@@ -8,7 +8,7 @@ do
 	do
 		head=$(lynx -source $i/|grep '<h1>' |awk -F'[<>]' '{print $3}')
 		url=$(lynx -source $i/|grep '/tinymce/jscripts/'|awk -F'"src":"' '{print $2}'|awk -F'"' '{print "http://www.daltai.com"$1}')
-		grep $url daltai.txt || printf "%08d\t%s\t%s\n" $sent "$head" $url >> daltai.txt
+		grep $url daltai.txt || printf "%08d\t%s\t%s\t%s\n" $sent $u "$head" $url >> daltai.txt
 		sent=$(($sent + 1))
 	done
 done
