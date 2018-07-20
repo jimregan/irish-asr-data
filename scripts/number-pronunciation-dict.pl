2d54a056adc03fd2955bb14f8ef96f0bd276deac
Jim O'Regan <jaoregan@tcd.ie>
2018-07-20 11:34:02 +0100

    add script

---
+++ scripts/number-pronunciation-dict.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use utf8;
+
+my %seen = ();
+
+while(<>) {
+	chomp;
+	my @parts = split/ /;
+	my $word = shift @parts;
+	if(exists $seen{$word}) {
+		$seen{$word}++;
+		print "$word($seen{$word}) " . join(" ", @parts) . "\n";
+	} else {
+		$seen{$word} = 1;
+		print "$word " . join(" ", @parts) . "\n";
+	}
+}
