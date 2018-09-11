ba9d35acad724adb15fdf93704e1c969f8cd9fc5
Jim O'Regan <jaoregan@tcd.ie>
2018-09-11 11:02:28 +0100

    normalisation updates

--- scripts/normalise.pl
+++ scripts/normalise.pl
@@ -19,12 +19,14 @@ binmode(STDOUT, ":utf8");
 #);
 
 my %norm = ();
-open(NORM, "$RealBin/normalisations.tsv");
+open(NORM, "$RealBin/normalisations.wiki");
 binmode(NORM, ":utf8");
 while(<NORM>) {
     chomp;
-    next if(/^#/);
-    my ($a, $b) = split/\t/;
+    next unless(/^\*/);
+    s/^\* //;
+    s/\/\/.*$//;
+    my ($a, $b) = split/: /;
     $norm{$a} = $b;
 }
 
@@ -37,37 +39,7 @@ while(<STDIN>) {
     s/!/ !/g;
     s/\?/ ?/g;
 
-    s/\bi n[-']aice /in aice /gi;
-    s/\ble n-a /lena /gi;
-    s/\b[Gg]ur ?bh’ ?/gurbh /gi;
-    s/\b[Gg]ur ?b’ ?/gurb /gi;
-    s/\b[Nn]ár bh’ ?/nárbh /gi;
-    s/\b[Dd]ár bh’ ?/dárbh /gi;
-    s/\b[Dd]’ár bh’ ?/dárbh /gi;
-    s/\b[Cc]ár bh’ ?/cárbh /gi;
-    s/\b[Nn]íor bh’ ?/níorbh /gi;
-    s/\bníb’ /níb'/gi;
-    s/\bmb’ fhéidir\b/mb'fhéidir/gi;
-    s/\bb’ fhéidir\b/b'fhéidir/gi;
-    s/\bm’ /m'/gi;
-    s/\bd’ /d'/gi;
-    s/\b[Cc]é ’r bh’ ?/cérbh /gi;
-    s/\bi n-aon /in aon/gi;
-    s/\b’dh eadh/dhea/gi;
-    s/\bb’ /b'/gi;
-    s/\bi n-Éirinn/in Éirinn/gi;
-    s/\bana bheag/an-bheag/gi;
-    s/\bana mhaith/an-mhaith/gi;
-    s/\bana mhór/an-mhór/gi;
-    s/\bn’ osguil/n'oscail/gi;
-    s/\bh-ana mhaith/han-mhaith/gi;
-    s/\bh-ana mhinic/han-mhinic/gi;
-    s/\bh-ana dheas/han-dheas/gi;
-    s/\bh-ana oban/han-oban/gi;
-    s/\bh-ana réidh/han-réidh/gi;
-    s/\bh-ana bhuartha/han-bhuartha/gi;
-
-    s/’/'/g;
+#    s/’/'/g;
 
     for my $word (split/ /) {
         my $lcword = irishlc($word);
