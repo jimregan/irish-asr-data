d676e53389a8d90f3916dc65140ce4df31ba6325
Jim O'Regan <jaoregan@tcd.ie>
2018-09-11 11:36:39 +0100

    readd vifax files

---
+++ vifax/extract-text.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+get_pdf_name() {
+	base=$(basename $1 '.mp4')
+	case $base in
+		20101123mean|20111109mean|20140311mean|20120320mean)
+			echo "$(basename $base 'mean')bun.pdf"
+			;;
+		*)
+			echo "$base.pdf"
+			;;
+	esac
+}
+
+
