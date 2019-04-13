db046c71cada7351f44de4ee2f24312c97d737c7
Jim Regan <jaoregan@tcd.ie>
2019-04-13 13:14:59 +0200

    Abair (#12)
    * add
    
    * Cois Life files via abair
    
    * cleaner for Harald's kaldi lexicon
    
    * ignore
    
    * IPA -> CMUdict -> (partial) abair.ie English phones
    
    * Cois Life soundcloud downloader
    
    * +1 MU
    
    * use hyphens to match Harald
    
    * old cleaner script
    
    * tweak
    
    * gen diff for finetuneas
    
    * add

--- scripts/coislife/guess-and-copy-pdfs.pl
+++ scripts/coislife/guess-and-copy-pdfs.pl
@@ -14,6 +14,16 @@ my %copyto = (
 	"njtGr95vD0p8ArHEb3P87A" => "an_tiriseoir_c1.pdf",
 	"8hMPtwGuxaf4hKqxim6qDA" => "i_dtir_mhilis_na_mbeo_c1.pdf",
 	"JDQ2kDJjz1vr1ecGGpbRCA" => "ibiotsa_c1.pdf",
+	"zZQnJOpzfv0uo8ZzX5Hy5A" => "an_fuioll_feа.pdf",
+	"pOz/E4O0xdcng8J5Has31A" => "an_tiriseoir.pdf",
+	"ietyOTxZ+WaTUj58s1sXWA" => "aois_fir.pdf",
+	"UJOe+lwqfemMJPiCk3ahDw" => "daideo.pdf",
+	"GwwBowDZBf9x1U2FNR3Rag" => "hata_zu_mhamo.pdf",
+	"5svNyA0mgyeSHqARGSoOAQ" => "i_dtir_mhilis_na_mbeo.pdf",
+	"CDMl7JTX9qiy5pW8ntvcgQ" => "oilithreach_pinn.pdf",
+	"ofMD2a3OCpd6SKgcym9WkA" => "seadna.pdf",
+	"/AEmvzuYC0/tapNWlfPoog" => "ulla.pdf",
+	"+U2JjZcFtyPivI3DmfwxOw" => "ibiotsa.pdf",
 );
 
 my %seen = ();
