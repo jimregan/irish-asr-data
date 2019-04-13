fb5a6101321368b58dbfaf8e2bbb803807139f5a
Jim O'Regan <jaoregan@tcd.ie>
2019-04-13 15:43:18 +0200

    Revert "use hyphens to match Harald, properly"
    This reverts commit a1bf66ae1936b6b491337f1c65dff2e4bc2311ee.

--- scripts/coislife/download-cl.sh
+++ scripts/coislife/download-cl.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-youtube-dl   
 youtube-dl https://soundcloud.com/cois-life-teoranta/01-b-otsa_dl-thph-irt-ocht-na -o ibiotsa-01.mp3
 youtube-dl https://soundcloud.com/cois-life-teoranta/02-b-otsa_cuairt-ghairid -o ibiotsa-02.mp3
 youtube-dl https://soundcloud.com/cois-life-teoranta/03-b-otsa_dalladh -o ibiotsa-03.mp3
@@ -10,49 +9,43 @@ youtube-dl https://soundcloud.com/cois-life-teoranta/07-b-otsa_pluais-an-tobac -
 youtube-dl https://soundcloud.com/cois-life-teoranta/08-b-otsa_an-mhaidhm -o ibiotsa-08.mp3
 youtube-dl https://soundcloud.com/cois-life-teoranta/09-b-otsa_seanchomr-daithe -o ibiotsa-09.mp3
 
-youtube-dl   
-youtube-dl https://soundcloud.com/cois-life-teoranta/1-cuairt-an-phapa-le-colm-o -o i-dtir-mhilis-na-mbeo-01.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/2-biorra-le-colm-o-ceallachain -o i-dtir-mhilis-na-mbeo-02.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/3-marta-a-tri-le-colm-o -o i-dtir-mhilis-na-mbeo-03.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/4-hale-bopp-le-colm-o -o i-dtir-mhilis-na-mbeo-04.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/5-i-le-colm-o-ceallachain -o i-dtir-mhilis-na-mbeo-05.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/6-i-measc-mhuilte-an-mhacnais -o i-dtir-mhilis-na-mbeo-06.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/i-gearrthog-o-i-dtir-mhilis-na-mbeo -o i-dtir-mhilis-na-mbeo-07.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/1-cuairt-an-phapa-le-colm-o -o i_dtir_mhilis_na_mbeo-01.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/2-biorra-le-colm-o-ceallachain -o i_dtir_mhilis_na_mbeo-02.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/3-marta-a-tri-le-colm-o -o i_dtir_mhilis_na_mbeo-03.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/4-hale-bopp-le-colm-o -o i_dtir_mhilis_na_mbeo-04.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/5-i-le-colm-o-ceallachain -o i_dtir_mhilis_na_mbeo-05.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/6-i-measc-mhuilte-an-mhacnais -o i_dtir_mhilis_na_mbeo-06.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/i-gearrthog-o-i-dtir-mhilis-na-mbeo -o i_dtir_mhilis_na_mbeo-07.mp3
 
-youtube-dl   
-youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-1-3_bayonne -o oilithreach-pinn-01.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn -o oilithreach-pinn-02.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-5-6_larasoana -o oilithreach-pinn-03.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-7-8_cirauqui -o oilithreach-pinn-04.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-9-11_viana-go -o oilithreach-pinn-05.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-12-13_santo -o oilithreach-pinn-06.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-14-16_ages-go -o oilithreach-pinn-07.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-17 -o oilithreach-pinn-08.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-20 -o oilithreach-pinn-09.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-23-25_leon-go -o oilithreach-pinn-10.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-26 -o oilithreach-pinn-11.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-29-31_o -o oilithreach-pinn-12.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-32 -o oilithreach-pinn-13.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-1-3_bayonne -o oilithreach_pinn-01.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn -o oilithreach_pinn-02.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-5-6_larasoana -o oilithreach_pinn-03.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-7-8_cirauqui -o oilithreach_pinn-04.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-9-11_viana-go -o oilithreach_pinn-05.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-12-13_santo -o oilithreach_pinn-06.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-14-16_ages-go -o oilithreach_pinn-07.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-17 -o oilithreach_pinn-08.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-20 -o oilithreach_pinn-09.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-23-25_leon-go -o oilithreach_pinn-10.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-26 -o oilithreach_pinn-11.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-29-31_o -o oilithreach_pinn-12.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/oilithreach-pinn-32 -o oilithreach_pinn-13.mp3
 
-youtube-dl   
-youtube-dl https://soundcloud.com/cois-life-teoranta/aois-fir-1-26 -o aois-fir-01.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/aois-fir-caibidl-5-9 -o aois-fir-02.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/aois-fir-c -o aois-fir-03.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/aois-fir-d -o aois-fir-04.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/aois-fir-1-26 -o aois_fir-01.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/aois-fir-caibidl-5-9 -o aois_fir-02.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/aois-fir-c -o aois_fir-03.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/aois-fir-d -o aois_fir-04.mp3
 
-youtube-dl   
 youtube-dl https://soundcloud.com/cois-life-teoranta/daideo-le-aine-ni-ghlinn-closleabhar-o-cois-life -o daideo.mp3
 
-youtube-dl   
-youtube-dl https://soundcloud.com/cois-life-teoranta/hata-zu-mhamo-caibidil-1-4 -o hata-zu-mhamo-01.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/hata-zu-mhamo-caibidil-5-10 -o hata-zu-mhamo-02.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/hata-zu-mhamo-caibidil-11-15 -o hata-zu-mhamo-03.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/hata-zu-mhamo-caibidil-1-4 -o hata_zu_mhamo-01.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/hata-zu-mhamo-caibidil-5-10 -o hata_zu_mhamo-02.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/hata-zu-mhamo-caibidil-11-15 -o hata_zu_mhamo-03.mp3
 
-youtube-dl   
-youtube-dl https://soundcloud.com/cois-life-teoranta/caibidil-1-an-tiriseoir -o an-tiriseoir-01.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/caibidil-2 -o an-tiriseoir-02.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/caibidil-3 -o an-tiriseoir-03.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/caibidil-4 -o an-tiriseoir-04.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/caibidil-5 -o an-tiriseoir-05.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/caibidil-6 -o an-tiriseoir-06.mp3
-youtube-dl https://soundcloud.com/cois-life-teoranta/caibidil-7 -o an-tiriseoir-07.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/caibidil-1-an-tiriseoir -o an_tiriseoir-01.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/caibidil-2 -o an_tiriseoir-02.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/caibidil-3 -o an_tiriseoir-03.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/caibidil-4 -o an_tiriseoir-04.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/caibidil-5 -o an_tiriseoir-05.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/caibidil-6 -o an_tiriseoir-06.mp3
+youtube-dl https://soundcloud.com/cois-life-teoranta/caibidil-7 -o an_tiriseoir-07.mp3
