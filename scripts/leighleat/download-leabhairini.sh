175cacf3fbbaafdb0217d30204a2b44eead483eb
Jim Regan <jaoregan@tcd.ie>
2019-09-10 22:15:21 +0200

    fix url

--- scripts/leighleat/download-leabhairini.sh
+++ scripts/leighleat/download-leabhairini.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in shuighh sa-teach mici thainig an-tra sa-chistin chuala sa-scoil sa-seomra-leaba
+for i in shuigh sa-teach mici thainig an-tra sa-chistin chuala sa-scoil sa-seomra-leaba
 do
 	swf=$(lynx -source http://leighleat.com/$i.html|grep '<param name="movie"'|awk -F'"' '{print $4}')
 	wget http://leighleat.com/$swf -O $i.swf
