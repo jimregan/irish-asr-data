d532590363d6c25f6614e4cdac321ac5b6587eda
Jim O'Regan <jaoregan@tcd.ie>
2019-09-26 16:45:45 +0200

    fixing

--- scripts/vifax-scraper.pl
+++ scripts/vifax-scraper.pl
@@ -9,11 +9,29 @@ use URI;
 use Web::Scraper;
 use Data::Dumper;
 
+my %seen = ();
+
 my $menu = scraper {
-#    process 'div[class="menu-cartlann-container"] ul[class="top-menu"] li#"menu-item-64" ul[class="sub-menu"] li a', 'pages[]' => '@href';
     process 'div[class="menu-cartlann-container"] ul li ul li a', 'pages[]' => '@href';
 };
 
-my $res = $menu->scrape(URI->new('http://vifax.maynoothuniversity.ie/'));
+my $videos = scraper {
+    process 'table tbody tr', 'videos[]' => scraper {
+        process 'td div[class="wp-video"] div div div video', 'video' => '@src';
+        process 'td a', 'pdfs[]' => '@href';
+    };
+};
 
-print Dumper $res;
+my $mainpage = URI->new('http://vifax.maynoothuniversity.ie/');
+my $curyear = URI->new('http://vifax.maynoothuniversity.ie/an-bhliain-acaduil-seo/');
+
+my $res = $menu->scrape($mainpage);
+
+print Dumper $curyear;
+process_page($curyear);
+
+sub process_page {
+    my $uri = shift;
+    my $pres = $videos->scrape($uri);
+    print Dumper $pres;
+}
