8403db35054e3d15f458cc2bf18c4868edc72141
Jim O'Regan <jaoregan@tcd.ie>
2021-09-28 14:37:10 +0100

    fix

--- scripts/daltai-proverbs-scraper.sh
+++ scripts/daltai-proverbs-scraper.sh
@@ -7,7 +7,7 @@ do
 	lynx -dump $url|grep "$url"|awk '{print $2}'|grep -v '/#'|grep -v "^$url$"|sort|uniq | while read i
 	do
 		head=$(lynx -source $i/|grep '<h1>' |awk -F'[<>]' '{print $3}'|head -n 1)
-		url=$(lynx -source $i/|grep '/tinymce/jscripts/'|awk -F'"src":"' '{print $2}'|awk -F'"' '{print "http://www.daltai.com"$1}'|head -n 1)
+		url=$(lynx -source $i/|grep '<audio'|awk -F'src="' '{print $2}'|awk -F'"' '{print "http://www.daltai.com"$1}'|head -n 1)
 		grep $url daltai.txt || printf "%08d\t%s\t%s\t%s\n" $sent $i "$head" $url >> daltai.txt
 		sent=$(($sent + 1))
 	done
